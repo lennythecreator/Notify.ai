1d4dc3741873ed2e96eb5694f8993738297fd7f1
Lenny Uwaeme <48912140+lennythecreator@users.noreply.github.com>
2024-07-29 16:38:59 -0400

    Updated Create.tsx

--- notify-client/src/layouts/create.tsx
+++ notify-client/src/layouts/create.tsx
@@ -1,13 +1,34 @@
 import React from 'react'
 import { Widget } from '../components/ui/widget'
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUpload } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
 export const Create = () => {
+  const [file, setFile] = useState(null)
+  const [fileName, setFileName] = useState("")
+  const [uploadedFile, setUploadedFile] = useState({})
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setFileName(file.name);
+    } else {
+      setFileName('Upload document');
+    }
+  }
+  
   return (
     <Widget>
         <h1 className="text-left text-xl">Create</h1>
-       <div className='flex items-center'>
+       <div className='flex items-center gap-4'>
              
-        <input type="file" name="" id="" placeholder='upload document' />
+       <div className="flex items-center gap-4">
+          <label htmlFor="file-upload" className="border-2 border-gray-300 rounded-[8px] h-11 px-4 flex items-center cursor-pointer hover:bg-gray-100">
+            <span className="text-gray-700">Upload document: {fileName} <FontAwesomeIcon icon={faUpload}/></span>
+          </label>
+          <input id="file-upload" type="file" className="hidden" onChange={handleFileChange}/>
+        </div>
+
         <button className='text-white'>Generate Note</button>
        </div>
     </Widget>
